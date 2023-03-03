bba82bc004df87a102393c6516d07331cb0e7c25
Matías Gutiérrez <matias.gutierrez85@gmail.com>
2023-03-02 22:33:46 -0300

    Segunda entrega

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, TextInput, View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola!! Coder</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.screen}>
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Item de lista" style={styles.input} />
+          <Button title="Agregar" />
+        </View>
+        <View style={styles.itemContainer}>
+          <Text style={styles.item}>Item A</Text>
+          <Text style={styles.item}>Item B</Text>
+          <Text style={styles.item}>Item C</Text>
+        </View>
+        <View style={styles.moreInfo}>
+          <Text>Descripción del Producto</Text>
+        </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 30,
   },
+  inputContainer: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: 200,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  itemContainer: {
+    marginTop: 30,
+  },
+  item: {
+    padding: 10,
+    textAlign: "left",
+  },
+  moreInfo:{
+    marginTop: 50,
+  }
 });
